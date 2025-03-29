04975b248745cf8a8d345fe8816b3bb056518239
ahmed <ahmedelkhodery2@gmail.com>
2025-03-29 22:12:48 +0200

    ktlint

--- CsvViewer/src/commonMain/kotlin/com/ryinex/kotlin/csvviewer/presentation/content/CSVOpener.kt
+++ CsvViewer/src/commonMain/kotlin/com/ryinex/kotlin/csvviewer/presentation/content/CSVOpener.kt
@@ -112,10 +112,10 @@ private fun Table(
                     name = if (isFirstHeader && first.isNotEmpty()) first[key] ?: key else key,
                     value = { _, data -> data.value[key]?.removeSurrounding("\"") ?: "" },
                     editTextConfig =
-                        editConfig.copy(onConfirmEdit = { data, _, text ->
-                            data.value[key] = text
-                            text
-                        })
+                    editConfig.copy(onConfirmEdit = { data, _, text ->
+                        data.value[key] = text
+                        text
+                    })
                 )
             }
 
